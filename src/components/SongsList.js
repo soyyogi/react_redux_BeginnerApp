cbcea3d88ec8460902e6cea43d33b67f32c1dc9f
soyyogi <raoyogesh78@yahoo.in>
2021-01-24 14:28:10 +0100

    added render list func to songslist component

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-function SongsList() {
+function SongsList(props) {
+
+    function renderList() {
+        return props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title} >
+                    <div className="right floated content" >
+                        <button className="ui button primary" >
+                            Select
+                        </button>
+                    </div>
+                    <div className="content" >{song.title}</div>
+                </div>
+            );
+        });
+    }
+
     return (
-        <div>
-            Songs List
+        <div className="ui divided list">
+            {renderList()}
         </div>
-    )
+    );
 }
 
 function mapStateToProps(state) {
